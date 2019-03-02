b5ed04c1019e2495d5a3df0a31b92d929af7da2e
Prathamesh <pprathamesh1594@gmail.com>
2019-03-02 21:07:14 +0530

    create Superuser/shopdatabase

--- shops/settings.py
+++ shops/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'nearbyshops'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'shops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'shops',
+        'USER': 'shopadmin',
+        'PASSWORD': 'shoppass',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
