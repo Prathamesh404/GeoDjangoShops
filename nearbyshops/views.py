63b21b5fefa8a7d65c7a3bdb6a87db3cd3dfab0e
Prathamesh <pprathamesh1594@gmail.com>
2019-03-03 22:28:59 +0530

    Minimal interface to show data

--- nearbyshops/views.py
+++ nearbyshops/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
+
+latitude =  28.5626728
+longitude = 89.2244577
+
+user_location = Point(longitude, latitude, srid=4326)
 # Create your views here.
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = "shops"
+    queryset = Shop.objects.annotate(distance=Distance("location", user_location)).order_by("distance")[0:2]
+    template_name = "shops/index.html"
